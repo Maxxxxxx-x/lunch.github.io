7ffe1b59e189a3c419c45cb775d3f43de3e007e4
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-11-04 00:09:42 +0800

    s

--- js/index.js
+++ js/index.js
@@ -2,24 +2,38 @@ window.onload=function(){
     var is =document.getElementsByClassName("is");
     var total=document.getElementById("total");
     var btn=document.getElementById("btn");
-
+    var rec=document.getElementById("rec");
+    var as=document.getElementsByClassName("as")
     
     btn.onclick=function(){
         var sum=0;
         for(var i=0;i<is.length;i++){
             sum+=(is[i].value)*70;
         }
-        total.innerText=sum;
+        var flag=confirm("是否確認送出？");
+        if(flag){
+            total.innerText=sum;
+            btn.disabled=true;
+            btn.innerText="不可再次提交";
+            var objArr=[
+                document.getElementById("name").value,
+                document.getElementById("num").value,
+                is[0].value,
+                is[1].value,
+                total.innerText,
+                document.getElementById("sel").value.split("T")[1],
+                document.getElementById("sel").value.split("T")[0],
+            ]
+            
+            for(var i=0;i<as.length;i++){
+                as[i].innerHTML=objArr[i];
+            }
+            rec.style.display="block";
+         }
 
-        var obj={
-            "name":document.getElementById("name").value,
-            "number":document.getElementById("num").value,
-            "time":document.getElementById("sel").value,
-            "meal":is[0].value,
-            "fruit":is[1].value,
-            "total":total.innerText,
-        };
+        
     };
+
 
     
 };
