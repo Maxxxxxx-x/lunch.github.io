002fb3a30569ab11aeca441ea84c685573831a12
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-11-03 23:29:00 +0800

    s

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-
+window.onload=function(){
+    var is =document.getElementsByClassName("is");
+    var total=document.getElementById("total");
+    var btn=document.getElementById("btn");
+    console.log(btn);
+    
+    btn.onclick=function(){
+        var sum=0;
+        for(var i=0;i<is.length;i++){
+            sum+=(is[i].value)*70;
+        }
+        total.innerText=sum;
+    };
+};
