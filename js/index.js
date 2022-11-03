e4a15d63176c95933cf892b18d69f96709ef64c3
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-11-04 00:27:52 +0800

    s

--- js/index.js
+++ js/index.js
@@ -3,16 +3,25 @@ window.onload=function(){
     var total=document.getElementById("total");
     var btn=document.getElementById("btn");
     var rec=document.getElementById("rec");
-    var as=document.getElementsByClassName("as")
+    var as=document.getElementsByClassName("as");
+    var main=document.getElementById("main");
+    var title=document.getElementById("title");
     
-    btn.onclick=function(){
-        var sum=0;
-        for(var i=0;i<is.length;i++){
-            sum+=(is[i].value)*70;
+    for(var i=0;i<is.length;i++){
+        is[i].onclick=function(){
+            var sum=0;
+            for(var j=0;j<is.length;j++){
+                sum+=(is[j].value)*70;
+                total.innerText=sum;
+            }
         }
+    }
+    btn.onclick=function(){
+        
+        
         var flag=confirm("是否確認送出？");
         if(flag){
-            total.innerText=sum;
+            
             btn.disabled=true;
             btn.innerText="不可再次提交";
             var objArr=[
@@ -29,6 +38,8 @@ window.onload=function(){
                 as[i].innerHTML=objArr[i];
             }
             rec.style.display="block";
+            main.style.display="none";
+            title.innerText="訂單明細";
          }
 
         
