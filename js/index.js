33a4a10adf0cb3e9898fb59a905bd6c87857135f
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-11-03 23:40:18 +0800

    s

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ window.onload=function(){
     var is =document.getElementsByClassName("is");
     var total=document.getElementById("total");
     var btn=document.getElementById("btn");
-    console.log(btn);
+
     
     btn.onclick=function(){
         var sum=0;
@@ -10,5 +10,16 @@ window.onload=function(){
             sum+=(is[i].value)*70;
         }
         total.innerText=sum;
+
+        var obj={
+            "name":document.getElementById("name").value,
+            "number":document.getElementById("num").value,
+            "time":document.getElementById("sel").value,
+            "meal":is[0].value,
+            "fruit":is[1].value,
+            "total":total.innerText,
+        };
     };
+
+    
 };
