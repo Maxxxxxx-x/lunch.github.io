e403796687b37ac768cd2330644a90a068b67002
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-11-04 00:52:27 +0800

    s

--- js/index.js
+++ js/index.js
@@ -11,24 +11,42 @@ window.onload=function(){
         is[i].onclick=function(){
             var sum=0;
             for(var j=0;j<is.length;j++){
-                sum+=(is[j].value)*70;
+                if(j<=1){
+                    sum+=(is[j].value)*70;
+                }
+                else{
+                    sum+=(is[j].value)*80;
+                }
+                
+                
                 total.innerText=sum;
             }
         }
     }
     btn.onclick=function(){
+        var objArr=[
+            document.getElementById("name").value,
+            document.getElementById("num").value,
+            is[0].value,
+            is[2].value,
+            is[1].value,
+            is[3].value,
+            total.innerText,
+            document.getElementById("sel").value.split("T")[1],
+            document.getElementById("sel").value.split("T")[0],
+        ]
         
-        
-        var flag=confirm("是否確認送出？");
+        var flag=confirm("是否確認送出？ 葷食："+objArr[2]+" 葷食(加蛋)："+objArr[3]+" 素食："+objArr[4]+" 素食（加蛋）："+objArr[5]+" 取餐時間："+objArr[7]);
         if(flag){
-            
             btn.disabled=true;
             btn.innerText="不可再次提交";
             var objArr=[
                 document.getElementById("name").value,
                 document.getElementById("num").value,
                 is[0].value,
+                is[2].value,
                 is[1].value,
+                is[3].value,
                 total.innerText,
                 document.getElementById("sel").value.split("T")[1],
                 document.getElementById("sel").value.split("T")[0],
