68e12819cc31aed08f8fa594ba02f913276ce291
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-11-04 14:10:38 +0800

    s

--- js/push.js
+++ js/push.js
@@ -92,9 +92,7 @@ const Demo = new Vue({
             document.getElementById("sel").value.split("T")[1],
             document.getElementById("sel").value.split("T")[0],
         ]
-        console.log("我是index 的flag")
-        console.log(document.getElementById("name").value.trim());
-        if(document.getElementById("name").value.trim()==""){
+        if(document.getElementById("name").value.trim()=="" || document.getElementById("name").value.trim()==undefined){
             alert("名字不可為空");
             return false;
         }
